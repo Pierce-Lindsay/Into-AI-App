7e989a32c295cde5b39ed5bf108c2e457677fa7c
Pierce Lindsay <ptlindsay@wpi.edu>
2025-11-30 22:14:42 -0500

    changed moved to main

--- grab_database.py
+++ grab_database.py
@@ -1,5 +1,5 @@
 import kagglehub
 # Download latest version
 path = kagglehub.dataset_download("nathanlauga/nba-games")
-
+#downloads to path printed out, will probably be at a cache folder
 print("Path to dataset files:", path)
