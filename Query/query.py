82694aff37857f1cba78f54eedf8ee862b1ad79f
Modularcontact16 <bachung2005@gmail.com>
2025-12-03 18:36:44 -0500

    Stubbing potential functions

---
+++ Query/query.py
@@ -0,0 +1,17 @@
+import string
+import pandas as pd
+
+def search_players(player: string):
+    pass
+
+def search_player_images(player: string):
+    pass
+
+def add_player_to_team(player: string):
+    pass
+
+def remove_player_from_team(player: string):
+    pass
+
+def predict_winrate():
+    pass
