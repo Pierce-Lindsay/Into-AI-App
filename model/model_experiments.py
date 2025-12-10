495c2a204e9189ad5547fa0578e54e24f7466d0a
Pierce Lindsay <ptlindsay@wpi.edu>
2025-12-09 19:30:35 -0500

    added first bad verion of model implementation

---
+++ model/model_experiments.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import make_scorer, matthews_corrcoef
+import models
+import joblib
+
+def average_zero_vals(df):
+    num_cols = df.select_dtypes(include="number").columns
+    column_means = df[num_cols].replace(0, np.nan).mean()
+    df[num_cols] = df[num_cols].replace(0, np.nan).fillna(column_means)
+    return df
+
+def update_best_model(model_save_path = "data/best_model.joblib"):
+    EXPER_SIZE = 0.01
+    EXPERIMENTING = True
+    RANDOM_STATE = 1
+    TRAIN_SIZE = 0.8
+    print("reading in csv...")
+    data_frame = pd.read_csv("data/player_stats_per_game.csv")
+    #clean up zero or NAN columns
+    print("cleaning csv...")
+    data_frame = data_frame.drop(data_frame.columns[0], axis = 1)
+    data_frame = data_frame[(data_frame.fillna(0) != 0).any(axis=1)]
+
+    Y = np.array(data_frame["GAME_WON"]) # grab labels
+    data_frame = data_frame.drop(["GAME_WON"], axis= 1)
+    data_frame = average_zero_vals(data_frame)
+    #average 0 values in columns later???? must be missing values, how can be 0???
+    X = pd.DataFrame(data_frame)
+
+    if EXPERIMENTING:
+        X, X_excluded, Y, Y_excluded = train_test_split(
+        X, Y,             
+        test_size=1-EXPER_SIZE,    
+        train_size=EXPER_SIZE,   
+        random_state=RANDOM_STATE,  
+        shuffle=True     
+        )
+        
+    X_train, X_test, Y_train, Y_test= train_test_split(
+        X, Y,             
+        test_size=1-TRAIN_SIZE,    
+        train_size=TRAIN_SIZE,   
+        random_state=RANDOM_STATE,  
+        shuffle=True     
+        )
+
+    models_to_test= [models.SVM(X_train, Y_train),
+                models.KNeighbors(X_train, Y_train), 
+                models.DecisionTree(X_train, Y_train),
+                models.LogRegression(X_train, Y_train)]
+    print("Training models...")  
+    #optimize models
+    for model in models_to_test:
+        model.optimize()
+
+    #print out table 1 data
+    print("Table 1: ")
+    print("Model: Best Perameters : MCC Score")
+    for model in models_to_test:
+        model.print_best_from_optimize()
+
+    #print out table 2 data
+    print("Table 2: ")
+    print("Model: Best Perameters : MCC Score")
+    for model in models_to_test:
+        model.update_best_predictions(X_test, Y_test)
+        
+     #find best score
+    max_model = max(models_to_test, key=lambda m: m.test_score)
+    print("Best model that should be used in the future:")
+    print(f"{max_model.name}: with test MCC: {max_model.test_score}!")
+    
+    joblib.dump(max_model.best_model, model_save_path)
+    
+    
+#update_best_model()
---
+++ model/models.py
@@ -0,0 +1,128 @@
+import numpy as np
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import make_scorer, matthews_corrcoef
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+
+#model constants
+K_SPLIT_COUNT = 5
+MAX_ITER = 1000
+#we want all scoring to be done according to MCC
+SCORER = make_scorer(matthews_corrcoef)
+
+#base class for models
+class Model:
+    def __init__(self, train_X, train_Y):
+        self.train_X = train_X
+        self.train_Y = train_Y
+        #filled in when functions called
+        self.grid = None
+        self.predicted = None
+        self.best_perams = None
+        self.best_score = None
+        self.test_score = None
+        self.best_model = None
+        self.name = "model"
+        
+    #optimize the model's perams for the given training data   
+    def optimize(self):
+        print("Fit not implemented!")
+    
+    #update the grid by fitting and setting important values  
+    def update_grid(self):
+        self.grid.fit(self.train_X, self.train_Y)
+        self.best_perams = self.grid.best_params_
+        self.best_score = self.grid.best_score_
+        self.best_model = self.grid.best_estimator_
+    
+    #print important values for training
+    def print_best_from_optimize(self):
+        print(f"{self.name}: {self.best_perams}: {self.grid.best_score_}")
+    
+    #update test_data prections values and print them
+    def update_best_predictions(self, test_X, test_Y):
+        #find the best peram combo and use it to get a score from test data
+        self.predicted = self.grid.best_estimator_.predict(test_X)
+        self.test_score = matthews_corrcoef(test_Y, self.predicted)
+        print(f"{self.name}: {self.best_perams}: {self.test_score}")
+        
+
+#wrapper class for fitting SVM
+class SVM(Model):
+    def __init__(self, train_X, train_Y):
+        super().__init__(train_X, train_Y)
+        self.name = "SVM" 
+
+    def optimize(self):
+        #param combinations  
+        param_grid = {'clf__C':[0.1, 1, 10], 
+                      'clf__gamma':[0.001,0.01, 0.1, 1], 
+                      'clf__kernel':['rbf', 'sigmoid', 'linear']}
+        
+        pipe = Pipeline([
+            ("scaler", StandardScaler()),
+            ("clf", SVC(probability=True,random_state=1, max_iter=MAX_ITER))])
+        
+        print("Fitting...SVM!")
+        self.grid = GridSearchCV(pipe, param_grid, scoring=SCORER, cv=K_SPLIT_COUNT)
+        self.update_grid()
+        
+#wrapper class for K nearest Neighbors    
+class KNeighbors(Model):
+    def __init__(self, train_X, train_Y):
+        super().__init__(train_X, train_Y)
+        self.name = "KNN" 
+            
+    def optimize(self):
+        #params to optimize
+        param_grid = {'clf__n_neighbors':[2, 4, 6, 8], 
+                      'clf__algorithm':['ball_tree', 'kd_tree', 'brute'], 
+                      'clf__p':[1, 2]}
+        pipe = Pipeline([
+        ("scaler", StandardScaler()),
+        ("clf", KNeighborsClassifier())])
+        
+        self.grid = GridSearchCV(pipe, param_grid, scoring=SCORER, cv=K_SPLIT_COUNT)
+        self.update_grid()                   
+
+#wrapper class for decision tree
+class DecisionTree(Model):
+    def __init__(self, train_X, train_Y):
+        super().__init__(train_X, train_Y)
+        self.name = "Decision Tree" 
+    
+    def optimize(self):   
+        #params to optimize
+        param_grid = {'clf__criterion':["gini", "entropy"], 
+                      'clf__max_depth':[None, 100, 10, 5], 
+                      'clf__ccp_alpha':[0.001, 0.01, 0.1]}
+        
+        pipe = Pipeline([
+        ("scaler", StandardScaler()),
+        ("clf", DecisionTreeClassifier(random_state=1))])
+        
+        self.grid = GridSearchCV(pipe, param_grid, scoring=SCORER, cv=K_SPLIT_COUNT)
+        self.update_grid()
+        
+#wrapper for logistic regression 
+class LogRegression(Model):
+    def __init__(self, train_X, train_Y):
+        super().__init__(train_X, train_Y)
+        self.name = "Logistic Regression"
+
+    def optimize(self):
+        #perams to optimize
+        param_grid = {'clf__penalty':[None, "l2"], 
+                      'clf__C':[0.01, 0.1, 1, 10], 
+                      'clf__solver':["lbfgs", "saga"]}
+        pipe = Pipeline([
+        ("scaler", StandardScaler()),
+        ("clf", LogisticRegression(max_iter=MAX_ITER))])
+        
+        self.grid = GridSearchCV(pipe, param_grid, scoring=SCORER, cv=K_SPLIT_COUNT)
+        self.update_grid()  
